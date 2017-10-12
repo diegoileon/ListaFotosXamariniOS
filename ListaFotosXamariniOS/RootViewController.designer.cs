d6da47295664efded72b4242b1d7ff56b3f7a8b2
diego ivan leon tapia <diego ivan leon tapia>
2017-10-12 09:30:02 -0500

    Agregar archivos de proyecto.

---
+++ ListaFotosXamariniOS/RootViewController.designer.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace ListaFotosXamariniOS
+{
+	[Register ("RootViewController")]
+	partial class RootViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
+
